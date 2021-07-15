11821b9bb102a587c567607ccd74d2d1302eb0a7
vls125 <vls125.25@gmail.com>
2021-07-15 15:52:36 +1000

    added shop, and details

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import {Route, Switch} from 'react-router-dom'
-
-const HatsPage=()=>{
-    return <div><h1>Hats</h1></div>
-}
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div>
             <Switch>
-                <Route exact path='/shop/hats' component={HatsPage}/>
+                <Route exact path='/shop' component={ShopPage}/>
                 <Route exact path='/' component={HomePage}/>
             </Switch>
         </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './homepage.styles.scss'
+import './homepage.style.scss'
 import Directory from "../../components/directory/directory.component";
 const HomePage=()=>(
     <div className="homepage">
